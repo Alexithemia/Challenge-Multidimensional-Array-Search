0a62ad4423f3a17ba291dbd4a237ba7830d86be0
Tyler <alexithemia@gmail.com>
2019-01-28 10:04:46 -1000

    Used locate function instead of new function in recursion

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 function locate(array, target) {
-  let found = false
-  function deepSearch(array, target) {
-    for (let i = 0; i < array.length; i++) {
-      if (typeof array[i] === 'object') {
-        deepSearch(array[i], target);
-      }
-      else {
-        if (array[i] === target) {
-          found = true;
-        }
+  let found = false;
+
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === 'object') {
+      found = locate(array[i], target);
+    }
+    else {
+      if (array[i] === target) {
+        found = true;
       }
     }
   }
-  deepSearch(array, target);
   return found;
 }
 
